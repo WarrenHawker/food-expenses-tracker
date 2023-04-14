ac30a7c77d4eb1d9958fc33c68fb4970849dc803
WarrenHawker <hawker.warren@gmail.com>
2023-04-14 09:00:45 +0100

    added expenses form

--- client/src/components/addExpense.tsx
+++ client/src/components/addExpense.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddExpense() {
+  const [companyInput, setCompanyInput] = useState<string>('');
+  const [amountInput, setAmountInput] = useState<number>();
+  const [dateInput, setDateInput] = useState<Date>();
+  const [notesInput, setNotesInput] = useState<string>('');
+
+  const handleFormSubmit = (e: React.FormEvent): void => {
+    e.preventDefault();
+  };
   return (
-    <>
-      <h2>add expenses form</h2>
-    </>
+    <section className='form-container'>
+      <h2>Add new Expense</h2>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor='company'>Bought from (shop or company)</label>
+        <input
+          type='text'
+          name='company'
+          value={companyInput}
+          onChange={(e) => setCompanyInput(e.target.value)}
+        />
+        <label htmlFor='amount'>Amount paid (£)</label>
+        <input
+          type='number'
+          name='amount'
+          value={amountInput}
+          onChange={(e) => setAmountInput(parseInt(e.target.value))}
+        />
+        <label htmlFor='date'>Date</label>
+        <input
+          type='date'
+          name='date'
+          value={dateInput?.toDateString()}
+          onChange={(e) => setDateInput(new Date(e.target.value))}
+        />
+        <label htmlFor='notes'>Notes</label>
+        <textarea
+          value={notesInput}
+          onChange={(e) => setNotesInput(e.target.value)}
+        />
+        <button>Add expense</button>
+      </form>
+    </section>
   );
 }
 
--- server/controllers/expensesController.js
+++ server/controllers/expensesController.js
@@ -2,6 +2,8 @@
 const getAllExpenses = () => {};
 
 //post new expense
-const createExpense = () => {};
+const createExpense = async (req, res) => {
+  const { company, date, amount, notes } = req.body;
+};
 
 export { getAllExpenses, createExpense };
