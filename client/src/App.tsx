f3a955177929cb1e593fe50392b02b166307b3fc
WarrenHawker <hawker.warren@gmail.com>
2023-04-14 07:02:57 +0100

    initial project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import AddExpense from './components/addExpense';
+import Expenses from './components/expenses';
+
+function App() {
+  return (
+    <>
+      <h1>Expenses Tracker App</h1>
+      <h2>By Warren Hawker</h2>
+      <AddExpense />
+      <Expenses />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/addExpense.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddExpense() {
+  return (
+    <>
+      <h2>add expenses form</h2>
+    </>
+  );
+}
+
+export default AddExpense;
